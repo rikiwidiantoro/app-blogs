d5cf7146ef446f651dcb179ac61fbfc55f39f802
Riki Widiantoro <rikitoro22@gmail.com>
2022-09-30 17:01:29 +0700

    factory article

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(mt_rand(3,6)),
+            'content' => $this->faker->paragraph(mt_rand(2,4)),
+            'image' => $this->faker->sentence(mt_rand(1)),
+            'category_id'
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        User::create([
+            'name' => 'Riki Widiantoro',
+            'email' => 'qwerty@aa.com',
+            'password' => bcrypt('qwertyuiop')
+        ]);
+        Article::create([
+            'title' => 'Judul Pertama',
+            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam magnam molestiae tenetur. Cupiditate quam exercitationem quae laborum doloremque dolores quod?',
+            'image' => 'image1.jpg',
+            'category_id' => 1
+        ]);
+        Article::create([
+            'title' => 'Judul Kedua',
+            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam magnam molestiae tenetur. Cupiditate quam exercitationem quae laborum doloremque dolores quod? At illum facere libero sequi, quis sit quibusdam minima quisquam voluptate, necessitatibus reiciendis alias qui assumenda consequatur eveniet repudiandae quia unde vitae temporibus rem quidem delectus. Animi dolorum odit qui?',
+            'image' => 'image2.jpg',
+            'category_id' => 1
+        ]);
+        Article::create([
+            'title' => 'Judul Ketiga',
+            'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam magnam molestiae tenetur. Cupiditate quam exercitationem quae laborum doloremque dolores quod? At illum facere libero sequi, quis sit quibusdam minima quisquam voluptate, necessitatibus reiciendis alias qui assumenda consequatur eveniet repudiandae quia unde vitae temporibus rem quidem delectus. Animi dolorum odit qui?',
+            'image' => 'image3.jpg',
+            'category_id' => 2
+        ]);
+        Category::create([
+            'name' => 'Kategori Satu',
+        ]);
+        Category::create([
+            'name' => 'Kategori Dua',
+        ]);
     }
 }
